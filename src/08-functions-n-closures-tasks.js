fad8dca62edd0818ab69109d61b6c74fa71167c6
s_iceman <s_iceman@protonmail.com>
2022-07-24 21:09:55 +0300

    feat: implement some tasks for functions

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -133,8 +133,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const param = args.length > 1 ? args.map((e) => `${JSON.stringify(e)}`).join(',') : `${args}`;
+    logFunc(`${func.name}(${param}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${param}) ends`);
+    return res;
+  };
 }
 
 
@@ -151,8 +157,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return (...rest) => fn(...[...args, ...rest]);
 }
 
 
@@ -173,8 +179,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return () => {
+    const res = counter;
+    counter += 1;
+    return res;
+  };
 }
 
 
