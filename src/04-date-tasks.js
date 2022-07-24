2c4b7bee93f1747c5d5926e4fad5b33f04fe2e81
s_iceman <s_iceman@protonmail.com>
2022-07-24 22:12:33 +0300

    feat: test implemented a task about the angle between the hands

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -96,16 +96,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  /*
-  const hours = (date.getHours() % 12) - 3;
+function angleBetweenClockHands(date, timeZoneShift = 3) {
+  const hours = (date.getHours() % 12) - timeZoneShift;
   let angle = Math.abs((60 * hours - 11 * date.getMinutes()) / 2);
-  if (angle > 180) {
-    angle = 360 - angle;
-  }
-  return (angle * Math.PI) / 180;
-  */
-  throw new Error('Not implemented');
+  if (angle > 180) { angle = 360 - angle; }
+  return (angle / 180) * Math.PI;
 }
 
 
